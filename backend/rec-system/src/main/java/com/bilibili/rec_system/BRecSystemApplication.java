9165b50c91510b8b194b30d0a87d6e1932349fd9
Jerry <1705077827@qq.com>
2025-11-17 13:41:19 +0800

    第一次上传

---
+++ backend/rec-system/src/main/java/com/bilibili/rec_system/BRecSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bilibili.rec_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BRecSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BRecSystemApplication.class, args);
+	}
+
+}
---
+++ backend/rec-system/src/test/java/com/bilibili/rec_system/BRecSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilibili.rec_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BRecSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
